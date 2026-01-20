45afa5a9db217812aecfd8e36fe01f7f17c62b0c
phanindra267 <151868018+phanindra267@users.noreply.github.com>
2026-01-20 14:12:24 +0530

    Add ConversionComponent for displaying converted code

--- src/components/ConversionComponent.jsx
+++ src/components/ConversionComponent.jsx
@@ -1,1 +1,17 @@
 
+import React from "react";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+const ConversionComponent = ({ convertedCode }) => {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-bold mb-2">Converted ES6 Code:</h2>
+      <SyntaxHighlighter language="javascript" style={oneDark}>
+        {convertedCode}
+      </SyntaxHighlighter>
+    </div>
+  );
+};
+
+export default ConversionComponent;
